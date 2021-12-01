71ed1d63b3f5b234b264e8da500674a471d01855
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2021-12-01 14:14:22 +0100

    challenge5 ternaire sans concatenation

--- challenge5.js
+++ challenge5.js
@@ -1,7 +1,7 @@
 const tipCalculator = (bill) => {
   let percentage;
   (bill >= 50 && bill < 300) ? percentage = 0.15 : percentage = 0.20;
-  return "La note était de " + bill + ", le pourboire de " + (percentage * bill) + " et la valeur totale était de " + (bill + percentage * bill) + ".";
+  return `La note était de ${bill}, le pourboire de ${percentage * bill} et la valeur totale était de ${bill + percentage * bill}.`;
 };
 
 console.log(tipCalculator(275));
