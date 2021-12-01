9cbc4d41e87f7602dddd9ea111ec71a9c2ab27aa
Matthias Lagrabe <matthias.lagrabe@gmail.com>
2021-12-01 14:37:33 +0100

    challenge 6

--- challenge5.js
+++
@@ -1,9 +0,0 @@
-const tipCalculator = (bill) => {
-  let percentage;
-  (bill >= 50 && bill < 300) ? percentage = 0.15 : percentage = 0.20;
-  return `La note était de ${bill}, le pourboire de ${percentage * bill} et la valeur totale était de ${bill + percentage * bill}.`;
-};
-
-console.log(tipCalculator(275));
-console.log(tipCalculator(40));
-console.log(tipCalculator(430));
